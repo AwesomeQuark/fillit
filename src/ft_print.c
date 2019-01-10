3157962bffd6b348ede93262f5fc1cd9835baa2a
Corentin NOEL <conoel@e2r8p21.42.fr>
2019-01-10 18:27:23 +0100

    try

--- src/ft_print.c
+++ src/ft_print.c
@@ -1,27 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_print.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: conoel <conoel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/10 18:05:58 by conoel            #+#    #+#             */
-/*   Updated: 2019/01/10 18:26:29 by conoel           ###   ########.fr       */
+/*   Created: 2019/01/10 18:23:22 by conoel            #+#    #+#             */
+/*   Updated: 2019/01/10 18:23:28 by conoel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fillit.h"
-
-int		main(int argc, char **argv)
-{
-	char	data[26][4][5];
-
-	if (argc != 2)
-	{
-		write(2, "usage : ./fillit [file name]\n", 29);
-		return (0);
-	}
-	data = load_data(argc, argv);
-	//data = fillit();
-	ft_print(data);
-}
